9cf59803cc602179c281f3941ebe34292f48bfa2
nageshgaikwad37 <107549162+nageshgaikwad37@users.noreply.github.com>
2022-11-09 20:57:35 +0530

    add para in faircard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FairlistComponent } from './shared/components/fairlist/fairlist.component';
+import { FaircardComponent } from './shared/components/fairlist/faircard/faircard.component';
+import { FairdetailsComponent } from './shared/components/fairdetails/fairdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FairlistComponent,
+    FaircardComponent,
+    FairdetailsComponent
   ],
   imports: [
     BrowserModule,
